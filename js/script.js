654a6761f685742ec245dab4515ba32087a4fef9
wafiz <wafi21z@hotmail.com>
2016-09-18 20:23:44 +0300

    fff

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var person1 = {
+	name : "Safa",
+	height : "2.20"
+};
+var person2 = {
+	name : "Diana",
+	height : "1.70"
+};
+var person3 = {
+	name : "dotan",
+	height : "1.80"
+};
+var persons = [];
+persons.push(person1);
+persons.push(person2);
+persons.push(person3);
+for (var i = 0; i < persons.length; i++) {
+persons[i].height=175;
+
+};
